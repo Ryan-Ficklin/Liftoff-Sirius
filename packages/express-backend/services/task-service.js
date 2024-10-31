b6836f0174bbb9200c4611ad60d2278f3a65efbd
Joshua Woo <113921587+jfwoo@users.noreply.github.com>
2024-10-31 16:58:02 -0700

    backend, hopefully add, find, and deleter tasks/users

---
+++ packages/express-backend/services/task-service.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import taskModel from "../models/task.js";
+
+mongoose.set("debug", true);
+
+mongoose
+  .connect("mongodb://localhost:27017/tasks", {
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error));
+
+function getTasks(name, description, due_date_time, priority) {
+  let promise;
+  promise = taskModel.find();
+  return promise;
+}
+
+function addTask(task) {
+  const taskToAdd = new taskModel(task);
+  const promise = taskToAdd.save();
+  return promise;
+}
+
+function findTaskByName(name) {
+  return userModel.find({ name: name });
+}
+
+function findTaskByPriority(priority) {
+  return userModel.find({ priority: priority });
+}
+
+const deleteTask = (name) => {
+    findOneandDelete({name: name});
+}
+
+
+export default {
+  addUser,
+  getTasks,
+  findTaskByName,
+  findTaskByPriority,
+  deleteTask,
+};
---
+++ packages/express-backend/services/user-service.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import userModel from "../models/user.js";
+
+mongoose.set("debug", true);
+
+mongoose
+  .connect("mongodb://localhost:27017/users", {
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error));
+
+function getUsers(username, password, task_list, email) {
+  let promise;
+  promise = userModel.find();
+  return promise;
+}
+
+function addUser(user) {
+  const userToAdd = new userModel(user);
+  const promise = userToAdd.save();
+  return promise;
+}
+
+const deleteUser = (username) => {
+    findOneandDelete({username: username});
+}
+
+function findUserByUsername(username) {
+  return userModel.find({ username: username });
+}
+
+function findUserByTask(task_list) {
+  return userModel.find({ task_list: task_list });
+}
+
+
+
+export default {
+  addUser,
+  getUsers,
+  findUserByUsername,
+  findUserByTask,
+  deleteUser,
+};
