d5082fc9a8022663b496ccbc0dd49ebc0b18cb92
Pranav Krishna <krishnatechpranav@gmail.com>
2024-11-01 14:03:26 -0700

    Added ui + backend signup & ui for login

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -2,15 +2,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TasksPage from "./pages/TasksPage";
 import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage"
 import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import { Toast } from 'primereact/toast';
+import { useRef } from 'react';
 
 function App() {
+    const toast = useRef(null);
+
+    const showToast = (severity, summary, detail) => {
+        toast.current.show({ severity, summary, detail });
+    };
+
+
     return (
         <PrimeReactProvider>
+            <Toast ref={toast} />
             <Router>
                 <Routes>
                     <Route path="/tasks" element={<TasksPage />} />
-                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/login" element={<LoginPage showToast={showToast}/>} />
+                    <Route path="/signup" element={<SignUpPage showToast={showToast}/>} />
                 </Routes>
             </Router>
         </PrimeReactProvider>
---
+++ packages/react-frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,121 @@
+import logo from "../assets/sirius-logo.svg";
+import "../SignUpPage.css";
+import { InputText } from "primereact/inputtext";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function LoginPage({ showToast }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [btnEnabled, setBtnEnabled] = useState(true);
+    const navigate = useNavigate();
+
+    function signup(){
+        setBtnEnabled(false)
+        if(username !== '' && password !== '' && email !== ''){
+
+            // User object
+            let user = {
+                "username": username,
+                "password": password,
+                "email": email,
+                "task_list": [],
+            }
+            console.log('works')
+
+            // Backend call
+            fetch("http://localhost:8000/users",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"  // Specify JSON format
+                    },
+                    body: JSON.stringify(user)
+                }
+            ).then(res => {
+                console.log(res)
+                if(res.status != 201){
+                    showToast('error', 'Error', 'Failed');
+                    return undefined;
+                } else {
+                    return res.json()
+                }
+            })
+            .then(data => {
+                setBtnEnabled(true)
+                console.log(data)
+                showToast('success', 'User created!', `The user ${username} was successfully created!`);
+                navigate('/login');
+
+            })
+            .catch((error) => {
+                setBtnEnabled(true)
+                //toast.current.show({ severity: 'error', summary: 'Error', detail: 'Failed: ' + error });
+                console.log(error)
+            })
+        } else {
+            setBtnEnabled(true)
+            //toast.current.show({ severity: 'warn', summary: 'Error', detail: 'One or more fields are left blank' });
+        }
+    }
+
+    return (
+        <div className="body">
+            <div className="d-flex justify-content-center login-section">
+                <div>
+                    <center>
+                        <img src={logo} className="img" />
+                    </center>
+                    <div className="card">
+                        <center>
+                            <h4 className="login-tag">Sign Up</h4>
+                        </center>
+
+                        {/* Username Input */}
+                        <p className="label">Username</p>
+                        <InputText
+                            id="username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            className="input"
+                        />
+
+                        {/* Email Input */}
+                        <p className="label">Email</p>
+                        <InputText
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="input"
+                            type="email"
+                        />
+
+                        {/* Password Input */}
+                        <p className="label">Password</p>
+                        <InputText
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="input"
+                            type="password"
+                        />
+
+                        <a href="/login">
+                            <p className="signup">Already have an account? Login</p>
+                        </a>
+
+                        <button className="signup-btn" onClick={signup} disabled={!btnEnabled}>Sign Up</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+// Validate the props
+LoginPage.propTypes = {
+    showToast: PropTypes.func.isRequired,  // expects a function
+};
+export default LoginPage;
